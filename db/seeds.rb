e3bb4aaf455a3813010b4cfa89f74c86db4507a4
Robert Fryman <robert.fryman@gmail.com>
2014-09-29 14:49:39 -0500

    added basic qualifications seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+basic_qualifications = [
+"Bowl filler operator",
+"Bowl caser Operator",
+"Bowl Relief operator",
+"Gallon Filler Operator",
+"Gallon Case Operator",
+"Quart Filler operator",
+"Quart Case Operator",
+"Liquid Pail Operator",
+"BIB Filler Operator",
+"Tote Filler Operator",
+"PW Pail Operator",
+"Rotary Operator",
+"CNC Operator",
+"On Top 8 Operator",
+"On Top 2 Operator",
+"Pudding Filler Operator",
+"T&I Relief Operator",
+"General Laborer with Fork Lift License",
+"General Laborer"
+]
+
+basic_qualifications.each do |name|
+  Qualification.create(name: name)
+end
